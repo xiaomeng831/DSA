3e9e38e6270573fe836d7016ed0e759a59a4d5fb
Brady <78976407+xiaomeng831@users.noreply.github.com>
2023-08-26 00:06:30 -0400

    Create Solution.py

---
+++ Set/1.Contains Duplicate (0217)/Solution.py
@@ -0,0 +1,10 @@
+# Set中的元素唯一
+# HashMap中的key唯一
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False 
