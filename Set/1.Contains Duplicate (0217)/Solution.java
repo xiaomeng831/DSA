21abaefadcce2877b9533d071ddce0b6b5267f46
Brady <78976407+xiaomeng831@users.noreply.github.com>
2023-08-26 00:05:38 -0400

    Create Solution.java

---
+++ Set/1.Contains Duplicate (0217)/Solution.java
@@ -0,0 +1,14 @@
+// HashSet中的元素唯一
+// HashMap中的key唯一
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hashSet = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++){
+	        if(hashSet.contains(nums[i])){
+		        return true;
+	        }
+	    hashSet.add(nums[i]);
+        }
+        return false;
+    }
+}
