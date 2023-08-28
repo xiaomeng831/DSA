d74b0d71088bda5b61efffb26e9567c07f27452c
Brady <78976407+xiaomeng831@users.noreply.github.com>
2023-08-28 00:28:37 -0400

    Create Solution.java

---
+++ HashTable/2.Two Sum (0001)/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> prev = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if(prev.isEmpty()){
+                prev.put(nums[i], i);
+            }else if(prev.containsKey(diff)){
+                return new int[]{prev.get(diff), i};
+            }else{
+                prev.put(nums[i], i);
+            }
+        }
+
+        return new int[2];
+    }
+}
