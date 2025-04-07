562a60f2b34e9eceea87dbd24f21b1b8951f7716
xiaomeng831 <bradyxu831@gmail.com>
2025-04-06 20:54:20 -0400

    class013 Code03

--- class013_Queue_Stack_CircularQueue/Code02_TwoKindsStacks.java
+++ class013_Queue_Stack_CircularQueue/Code02_TwoKindsStacks.java
@@ -5,60 +5,59 @@ import java.util.Stack;
 public class Code02_TwoKindsStacks {
 
     // Java内部动态数组实现, 常数操作慢
-    public static class Stack1{
+    public static class Stack1 {
         public Stack<Integer> stack = new Stack<>();
 
-        public boolean isEmpty(){
+        public boolean isEmpty() {
             return stack.isEmpty();
         }
 
-        public void push(int num){
+        public void push(int num) {
             stack.push(num);
         }
 
-        public int pop(){
+        public int pop() {
             return stack.pop();
         }
 
-        public int peek(){
+        public int peek() {
             return stack.peek();
         }
 
-        public int size(){
+        public int size() {
             return stack.size();
         }
 
     }
 
-    public static class Stack2{
+    public static class Stack2 {
         public int[] stack;
         public int size;
 
-        public Stack2(int n){
+        public Stack2(int n) {
             stack = new int[n];
             size = 0;
         }
 
-
-        public boolean isEmpty(){
+        public boolean isEmpty() {
             return size == 0;
         }
 
-        public void push(int num){
+        public void push(int num) {
             stack[size++] = num;
         }
 
-        public int pop(){
+        public int pop() {
             return stack[--size];
         }
 
-        public int peek(){
+        public int peek() {
             return stack[size - 1];
         }
 
-        public int size(){
+        public int size() {
             return size;
-        }       
+        }
 
     }
 
